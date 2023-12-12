6ca6297550fd0af5a5655e625eff34a005e44691
Nader Abdulrub <nabdulrub16@gmail.com>
2023-12-11 22:31:23 -0500

    git: conflict fix

--- src/components/branding/Logo.tsx
+++ src/components/branding/Logo.tsx
@@ -8,13 +8,9 @@ type LogoProps = {
 
 const Logo = ({ className }: LogoProps) => {
   return (
-<<<<<<< HEAD
     <span className={twMerge("text-4xl text-white font-semibold", className)}>
       {agent.info.firstname}
     </span>
-=======
-    <div className={twMerge("text-6xl text-white", className)}>BNGOON</div>
->>>>>>> ff7163c124d25b9f811bb23b1a74a3a3decb9ecf
   );
 };
 
