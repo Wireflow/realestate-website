f2495b00033b7f3dbd79f9c0e74d2f70689692e3
Nader Abdulrub <nabdulrub16@gmail.com>
2023-12-11 22:37:39 -0500

    fix: added max width of 1920px for layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
+import { twMerge } from "tailwind-merge";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -20,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>
+      <body className={twMerge("max-w-[1920px] mx-auto", poppins.className)}>
         <Navbar />
         {children}
       </body>
