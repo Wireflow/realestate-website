7738a7b77f178f3322902fff2f93b3dd3667008a
kuroafa <kuroafa21@gmail.com>
2023-12-11 23:14:14 -0500

    feat: polish testimonial

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,34 +1,17 @@
-'use client'
+"use client";
 import { StarIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
 import TestimonialHome from "../../public/TestimonialHome.jpg";
 import TestimonialMobile from "../../public/TestimonialMobile.jpg";
-
 
 type Props = {};
 
 const Testimonials = (props: Props) => {
-  const [isMobile, setIsMobile] = useState(false);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 1280); // Adjust the breakpoint as needed
-    };
-
-    // Initial check
-    handleResize();
-
-    // Listen for window resize events
-    window.addEventListener("resize", handleResize);
-
-    // Cleanup
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
 
   return (
     <div className="bg-[#F2F5FF]  ">
-      <div className="flex justify-center flex-col pl-5 xl:gap-[700px] lg:gap-[500px] md:gap-[250px] md:flex-row md:items-center pt-10 ">
+      <div className="flex justify-between px-[390px] flex-col md:flex-row md:items-center pt-10 ">
         <div className="flex flex-col gap-3">
           <h1 className="font-bold text-[32px]">Hear What They Say</h1>
           <p className="text-md font-medium text-gray-400">
@@ -37,19 +20,16 @@ const Testimonials = (props: Props) => {
         </div>
         <p className="font-bold mt-2 ">See All Testimonials</p>
       </div>
-      <div className="flex justify-center items-center flex-col xl:flex-row gap-[40px] h-full py-10">
+      <div className="flex justify-center items-center flex-col xl:flex-row gap-[20px] h-full py-10">
         <div className="  px-5  text-white rounded-xl ">
-          <Image
-            className="rounded-xl xl:w-[500px] xl:h-[600px] "
-            alt="Testimonial-Home-Image"
-            src={isMobile ? TestimonialMobile : TestimonialHome}
-            width={600}
-            height={650}
-          />
+          <picture>
+            <source width={400}  srcSet="TestimonialHome.jpg" media="(min-width: 1280px)" />
+            <img src="TestimonialMobile.jpg" alt="MDN" />
+          </picture>
         </div>
         <div className=" ">
-          <div className=" flex flex-col sm:flex-row gap-x-10 gap-10">
-            <div className="bg-[#FFF] rounded-xl p-10 sm:w-80 w-[400px] ">
+          <div className=" flex flex-col sm:flex-row gap-x-5 gap-10">
+            <div className="bg-[#FFF] rounded-xl p-10 sm:w-[400px] w-[450px] ">
               <div className="flex gap-2">
                 <StarIcon className="w-5 h-12" />
                 <StarIcon className="w-5 h-12" />
@@ -66,7 +46,7 @@ const Testimonials = (props: Props) => {
                 <h1 className="font-bold text-2xl">Marc Pillay</h1>
               </div>
             </div>
-            <div className="bg-[#FFF] rounded-xl p-10 sm:w-80 w-[400px]">
+            <div className="bg-[#FFF] rounded-xl p-10 sm:w-[400px] w-[450px]">
               <div className="flex gap-2">
                 <StarIcon className="w-5 h-12" />
                 <StarIcon className="w-5 h-12" />
@@ -84,8 +64,8 @@ const Testimonials = (props: Props) => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col sm:flex-row  gap-x-10 gap-10 pt-5">
-            <div className="bg-[#FFF] rounded-xl p-10 sm:w-80 w-[400px]">
+          <div className="flex flex-col sm:flex-row  gap-x-5 gap-10 pt-5">
+            <div className="bg-[#FFF] rounded-xl p-10 sm:w-[400px] w-[450px]">
               <div className="flex gap-2">
                 <StarIcon className="w-5 h-12" />
                 <StarIcon className="w-5 h-12" />
@@ -102,7 +82,7 @@ const Testimonials = (props: Props) => {
                 <h1 className="font-bold text-2xl">Marc Pillay</h1>
               </div>
             </div>
-            <div className="bg-[#FFF] rounded-xl p-10 sm:w-80 w-[400px] ">
+            <div className="bg-[#FFF] rounded-xl p-10 sm:w-[400px] w-[450px] ">
               <div className="flex gap-2">
                 <StarIcon className="w-5 h-12" />
                 <StarIcon className="w-5 h-12" />
